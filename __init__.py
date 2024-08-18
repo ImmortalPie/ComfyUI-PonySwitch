23e38504c409d2416a323b7a906fd28154e535f0
ImmortalPie <theimmortalpie@gmail.com>
2024-08-18 15:05:07 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .PonySwitch import PonySwitch 
+NODE_CLASS_MAPPINGS = { "PonySwitch" : PonySwitch }
+NODE_DISPLAY_NAME_MAPPINGS = { "PonySwitch" : "PonySwitch" }
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
