027b22ba08928ebaaa2f1fd634bdd9d7b8d644f5
ImmortalPie <theimmortalpie@gmail.com>
2024-08-18 13:43:00 +0100

    testing

--- ComfyUI-PonySwitch/PonySwitch.py
+++ ComfyUI-PonySwitch/PonySwitch.py
@@ -5,16 +5,16 @@ class PonySwitch:
     def INPUT_TYPES():
         return {
             "required": {
-                "Toggle Switch": ("BOOLEAN", {"forceInput": False}),
+                "Toggle Switch": ("BOOLEAN", {"forceInput": True}),
                 "Positive Pony Tags": ("STRING", {"default": "score_9, score_8_up, score_7_up, score_6_up", "multiline": True}),
                 "Negative Pony Tags": ("STRING", {"default": "score_1, score_2, score_3, score_4, score_5, score_6", "multiline": True}),
-                "Positive": ("STRING", {"default": '', "forceInput": True}),
-                "Negative": ("STRING", {"default": '', "forceInput": True}),
+                "Positive Prompt": ("STRING", {"default": '', "forceInput": True}),
+                "Negative Prompt": ("STRING", {"default": '', "forceInput": True}),
             }
         }
 
     RETURN_TYPES = ("STRING", "STRING")
-    RETURN_NAMES = ("Positive", "Negative")
+    RETURN_NAMES = ("Modified Positive Prompt", "Modified Negative Prompt")
     OUTPUT_IS_LIST = (False, False)
     FUNCTION = "compute"
 
@@ -25,23 +25,23 @@ class PonySwitch:
         toggle_switch = inputs["Toggle Switch"]
         positive_pony_tags = inputs["Positive Pony Tags"]
         negative_pony_tags = inputs["Negative Pony Tags"]
-        positive = inputs["Positive"]
-        negative = inputs["Negative"]
+        positive_prompt = inputs["Positive Prompt"]
+        negative_prompt = inputs["Negative Prompt"]
 
         if toggle_switch:
             # Split tags into a list and strip whitespace
             positive_pony_tags = [tag.strip() for tag in positive_pony_tags.split('\n')]
             negative_pony_tags = [tag.strip() for tag in negative_pony_tags.split('\n')]
 
-            # Add pony tags at the beginning of the positive and negative  s with two new lines after
-            positive = " ".join(positive_pony_tags) + "\n\n" + positive
-            negative = " ".join(negative_pony_tags) + "\n\n" + negative
+            # Add pony tags at the beginning of the positive and negative prompts with two new lines after
+            modified_positive_prompt = " ".join(positive_pony_tags) + "\n\n" + positive_prompt
+            modified_negative_prompt = " ".join(negative_pony_tags) + "\n\n" + negative_prompt
         else:
-            # If toggle switch is off, return original  s
-             positive = positive
-             negative = negative
+            # If toggle switch is off, return original prompts
+            modified_positive_prompt = positive_prompt
+            modified_negative_prompt = negative_prompt
 
-        return (positive, negative)
+        return (modified_positive_prompt, modified_negative_prompt)
 
 # Required mapping for ComfyUI to recognize the node
 NODE_CLASS_MAPPINGS = {
